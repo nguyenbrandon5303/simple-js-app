d27c2a5e9e6cec58b0c7262b3d0007131d107024
Brandon Nguyen <51946258+nguyenbrandon5303@users.noreply.github.com>
2021-04-02 12:47:27 -0700

    created pokemon list of 3 pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-alert('Hello world');
-let favoriteFood = 'orange';
-document.write(favoriteFood);
+let pokemonList = [];
+
+let gengar = {
+  name: 'Gengar',
+  height: 1.5,
+  type: ['Ghost', 'Poison']
+};
+
+let machamp = {
+  name: 'Machamp',
+  height: 1.6,
+  type: ['Fighting']
+};
+
+let dragonite = {
+  name: 'Dragonite',
+  height: 2.2,
+  type: ['Dragon', 'Flying']
+};
+
+pokemonList = [gengar, machamp, dragonite];
