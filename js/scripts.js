fde166d183f11ca0ddaf2fd0162c1abd70cc2b43
Brandon Nguyen <51946258+nguyenbrandon5303@users.noreply.github.com>
2021-04-06 11:35:43 -0700

    updated using IIFE for repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,33 @@
-let pokemonList = [];
+let pokemonRepository = (function () {
+  let pokemonList = [
+  { name: 'Gengar',
+    height: 1.5,
+    type: ['Ghost', 'Poison']
+  },
+  { name: 'Machamp',
+    height: 1.6,
+    type: ['Fighting']
+  },
+  { name: 'Dragonite',
+    height: 2.2,
+    type: ['Dragon', 'Flying']
+  }];
 
-let gengar = {
-  name: 'Gengar',
-  height: 1.5,
-  type: ['Ghost', 'Poison']
-};
+  function getAll() {
+    return pokemonList;
+  }
 
-let machamp = {
-  name: 'Machamp',
-  height: 1.6,
-  type: ['Fighting']
-};
+  function add(item) {
+    pokemonList.push(item);
+  }
 
-let dragonite = {
-  name: 'Dragonite',
-  height: 2.2,
-  type: ['Dragon', 'Flying']
-};
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
 
-pokemonList = [gengar, machamp, dragonite];
-
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.height >= 1.6) {
     document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') ' + '- Wow, that\'s big!' + '</p>');
   } else {
